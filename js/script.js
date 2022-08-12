b69e7d3283a74335033c96ea17a5e652845f3ac1
Artem Yasinscky <artemyasinscky@gmail.com>
2022-08-12 09:32:43 +0400

    update files

--- js/script.js
+++ js/script.js
@@ -226,6 +226,50 @@ class MenuCard {
         'menu__item',
 
     ).render();
+
+    // Forms 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: "Загрузка",
+        success: "Спасибо!Скоро с Вами свяжемся",
+        failure: "Что-то пошло не так..."
+    };
+
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage =document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+
+            request.setRequestHeader('Content-type', 'multipart/form-data')
+            const formData = new FormData(form);
+
+            request.send(formData);
+
+
+            request.addEventListener('load', () => {
+                if (request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+
+
+
 });
 
 
